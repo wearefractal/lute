ce68891b378986893fe15a74c8f8efdf1d7a4e57
Joe Fleming <joe.fleming@gmail.com>
2014-04-19 17:56:27 -0700

    refactor: pass in open, pass in argv as options

--- bin/lute.js
+++ bin/lute.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-var argv  = require('minimist')(process.argv.slice(2));
-var isInt = require('../lib/util/isInt');
-var serve = require('../lib/serve');
-var port  = (argv.p && isInt(argv.p)) ? argv.p : 8080;
+var argv        = require('minimist')(process.argv.slice(2));
+var isInt       = require('../lib/util/isInt');
+var serve       = require('../lib/serve');
+var port        = (argv.p && isInt(argv.p)) ? argv.p : 8080;
+var browserOpen = (argv._.indexOf('open') !== -1);
 
-serve(port, argv);
+serve(port, browserOpen, argv);
