8beed14b899df2b434790e900f2c6bc4520a0ad6
Joe Fleming <joe.fleming@gmail.com>
2014-04-19 17:45:42 -0700

    allow ignored paths to be passed in

--- bin/lute.js
+++ bin/lute.js
@@ -5,5 +5,4 @@ var isInt = require('../lib/util/isInt');
 var serve = require('../lib/serve');
 var port  = (argv.p && isInt(argv.p)) ? argv.p : 8080;
 
-// if a port or no args, start server
-serve(port, (argv._.indexOf('open') !== -1));
+serve(port, (argv._.indexOf('open') !== -1), argv);
