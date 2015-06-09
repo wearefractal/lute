e29cb37a9e3df78d2a160ae82f6d5206ad37ddae
Joe Fleming <joe.fleming@gmail.com>
2015-06-09 14:12:16 -0700

    whitespace changes

--- bin/lute.js
+++ bin/lute.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var argv         = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
 var parseOptions = require('../lib/util/parseOptions');
-var serve        = require('../lib/serve');
+var serve = require('../lib/serve');
 
 serve(parseOptions(argv));
